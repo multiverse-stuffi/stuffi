a6817460e95683f0fe6f695e5f933e70c9958c59
Reese0177 <vinnygo@gmail.com>
2023-01-16 12:34:20 -0600

    Initial commit

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Stuffi</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Organize, filter, sort, and rate items" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
