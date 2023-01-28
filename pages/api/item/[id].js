560edf6f2a7df0da918133979bb4347c5e01b7e6
Reese0177 <vinnygo@gmail.com>
2023-01-28 08:41:42 -0600

    Add item PUT endpoint

---
+++ pages/api/item/[id].js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken');
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method === 'PUT') {
+        const item = req.body.item;
+        const imgUrl = req.body.imgUrl;
+        const description = req.body.description;
+        let id = 0;
+
+        if (isNaN(req.query.id)) {
+            res.status(400).send('ID must be a number');
+            return;
+        } else id = Number(req.query.id);
+        
+        if (!item.trim()) {
+            res.status(400).send('Name required');
+            return;
+        }
+
+        if (req.cookies.token) jwt.verify(req.cookies.token, process.env.NEXT_PUBLIC_JWT_SECRET, async function (err, decoded) {
+            if (!decoded.id) {
+                res.status(403).send('Not Authorized');
+                return;
+            }
+
+            try {
+                const db_item = await prisma.item.findUnique({
+                    where: {
+                        id
+                    }
+                });
+                if (db_item.userId !== decoded.id) {
+                    res.status(403).send('Not Authorized');
+                    return;
+                }
+                const updated = await prisma.item.update({
+                    data: {
+                        item,
+                        imgUrl,
+                        description
+                    },
+                    where: {
+                        id
+                    }
+                })
+                res.status(200).json(updated);
+            } catch (e) {
+                console.log(e);
+                res.status(500).send('Server Error');
+            }
+            
+        });
+        else res.status(403).send('Not Authorized');
+    }
+}
