b88d85fbb2830db22ec19c8f5802cdf619b66b4a
Reese0177 <vinnygo@gmail.com>
2023-01-27 18:01:22 -0600

    Add ability to change password

---
+++ pages/api/user/signup/[id].js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+import { cookieParser } from 'cookie-parser';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method === 'PUT') {
+        const oldPassword = req.body.oldPassword ?? '';
+        const newPassword = req.body.newPassword ?? '';
+        if (!newPassword.trim() || !oldPassword.trim()) {
+            res.status(400).send('Passwords cannot be blank');
+            return;
+        }
+        if (newPassword === oldPassword) {
+            res.status(400).send('New password cannot be the same as the old');
+            return;
+        }
+        if (newPassword.length < 8 || !/[a-z]/.test(newPassword) || !/[A-Z]/.test(newPassword) || !/\d/.test(newPassword)) {
+            res.status(400).send('New password must be at least 8 characters and contain uppercase, lowercase, and a number');
+            return;
+        }
+        if (req.cookies.token) jwt.verify(req.cookies.token, process.env.NEXT_PUBLIC_JWT_SECRET, async function(err, decoded) {
+            try {
+                const id = decoded.id;
+                const user = await prisma.user.findUnique({where: {id}});
+                if (user) {
+                    bcrypt.compare(oldPassword, user.password, async function(err, result) {
+                        if (result) {
+                            bcrypt.hash(newPassword, 10, async function(err, hash) {
+                                const user = await prisma.user.update({
+                                    data: {
+                                        password: hash
+                                    },
+                                    where: {
+                                        id
+                                    }
+                                });
+                                res.status(200).send('success');
+                            });
+                        } else res.status(400).send('Incorrect password');
+                    });
+                } else res.status(400).send('User not found');
+            } catch (e) {
+                res.status(500).send('Server Error');
+            }
+        });
+        else res.status(403).send('Not Authorized');
+    }
+}
