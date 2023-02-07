d23b069d589615849afac35b336c8a07b718401f
Reese0177 <vinnygo@gmail.com>
2023-02-07 17:41:46 -0600

    Begin log in button and modal

--- components/LogButton.jsx
+++ components/LogButton.jsx
@@ -1,23 +1,85 @@
 'use client';
 import {getCookie, deleteCookie} from 'cookies-next';
 import {useState} from 'react';
-import styles from '../styles/LogButton.module.css';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%', 
+    transform: 'translate(-50%, -50%)',
+    padding: '10px'
+  },
+};
+
+Modal.setAppElement('#__next');
 
 export default function logButton() {
     
     const [token, setToken] = useState(getCookie('token'));
-    const handler = async () => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorText, setErrorText] = useState('');
+    const [isNew, setIsNew] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal(){
+
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const clickHandler = () => {
         if (token) {
             deleteCookie('token');
             setToken(null);
         } else {
-            const res = await fetch('/api/user/login', {method: 'POST', body: JSON.stringify(data)});
-            setToken(getCookie('token'));
+            openModal();
         }
     };
+
+    const logInHandler = async (e) => {
+        e.preventDefault();
+        if (!username.trim() || !password.trim() || password.length < 8 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/\d/.test(password)) {
+            setErrorText('Username and valid password required');
+            return;
+        }
+        await fetch(`/api/user/${isNew ? 'signup' : 'login'}`, {method: 'POST', body: JSON.stringify({username, password})});
+        setToken(getCookie('token'));
+        setUsername('');
+        setPassword('');
+        closeModal();
+    }
+
     return (
-        <a onClick={handler}>
-            {token ? 'log out' : 'log in'}
-        </a>
+        <>
+            <a className='button' onClick={clickHandler}>
+                {token ? 'Log out' : 'Log in'}
+            </a>
+            <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Log In Modal"
+            >
+                <form className='modal' onSubmit={logInHandler}>
+                    <input value={username} name="username" onChange={(ev)=>{setUsername(ev.target.value); setErrorText('');}} placeholder="Username"></input>
+                    <input value={password} name="password" onChange={(ev)=>{setPassword(ev.target.value); setErrorText('');}} placeholder="Password" type="password"></input>
+                    <span className={"error" + (!errorText ? ' hidden' : '')}>{errorText}</span>
+                    <button>{isNew ? 'Sign up' : 'Log in'}</button>
+                    <a onClick={()=>setIsNew(!isNew)}>{isNew ? 'Already have an account?' : 'New here?'}</a>
+                </form>
+            </Modal>
+        </>
     );
 }
