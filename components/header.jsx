0642ea2150fff07e8d6332ff3b88b858d1a677a9
Reese0177 <vinnygo@gmail.com>
2023-02-10 15:58:20 -0600

    Fix Head

--- components/header.jsx
+++ components/header.jsx
@@ -4,28 +4,32 @@ import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import LogButton from "./LogButton";
 import Image from 'next/image';
+import Head from './head';
 
 function Header(props) {
   return (
-    <Box>
-      <AppBar
-        position="static"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          background: "#004F2D",
-          padding: "15px 1em"
-        }}
-      >
-        <Box sx = {{display: "flex", alignItems: "center"}}>
-        <Image src="/thumbtack.png" width={50} height={50} alt="logo"/>
-        <Typography variant="h4">Stuffi</Typography>
-        </Box>
-        <LogButton refreshData={props.refreshData}/>
-      </AppBar>
+    <>
+      <Head />
+      <Box>
+        <AppBar
+          position="static"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            background: "#004F2D",
+            padding: "15px 1em"
+          }}
+        >
+          <Box sx = {{display: "flex", alignItems: "center"}}>
+          <Image src="/thumbtack.png" width={50} height={50} alt="logo"/>
+          <Typography variant="h4">Stuffi</Typography>
+          </Box>
+          <LogButton refreshData={props.refreshData}/>
+        </AppBar>
     </Box>
+    </>
   );
 }
 
