cc2ee12dbf3b9a7397724bd8369f931f90878410
Reese0177 <vinnygo@gmail.com>
2023-02-18 17:41:46 -0600

    Fix env vars for AWS

--- pages/api/item/index.js
+++ pages/api/item/index.js
@@ -34,15 +34,15 @@ export default async function handler(req, res) {
                         size: "512x512"
                     });
                     const s3 = new AWS.S3({
-                      accessKeyId: process.env.AWS_ACCESS_KEY,
-                      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                      accessKeyId: process.env.AMZ_ACCESS_KEY,
+                      secretAccessKey: process.env.AMZ_SECRET_ACCESS_KEY,
                     });
 
                     const imgRes = await fetch(response.data.data[0].url);
                     const arrBuffer = await imgRes.arrayBuffer();
                     const buffer = Buffer.from(arrBuffer);
                     const uploadedImg = await s3.upload({
-                        Bucket: process.env.AWS_S3_BUCKET_NAME,
+                        Bucket: process.env.AMZ_S3_BUCKET_NAME,
                         Key: item+Date.now()+'.png',
                         Body: buffer
                     }).promise();
