0baf5d37cc3bd4c1dd3f93d029abcf213ae3353f
Jen Perez <jenperez613@icloud.com>
2023-02-24 06:57:00 -0500

    chatgpt told me how to do all of this

--- pages/api/items/[id]/index.js
+++ pages/api/items/[id]/index.js
@@ -1,135 +1,111 @@
-const jwt = require('jsonwebtoken');
 import prisma from '@/lib/prisma';
+import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
-    if (req.method === 'PUT') {
-        req.body = JSON.parse(req.body);
-        const item = req.body.item ? req.body.item.trim() : null;
-        let imgUrl = req.body.imgUrl ? req.body.imgUrl.trim() : null;
-        const description = req.body.description ? req.body.description.trim() : null;
-        const url = req.body.url ? req.body.url.trim() : null;
-        let id = 0;
+  // make sure user is authenticated
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  import prisma from '@/lib/prisma';
 
-        if (isNaN(req.query.id)) {
-            res.status(400).send('ID must be a number');
-            return;
-        } else id = Number(req.query.id);
+  export default async function handler(req, res) {
+    if (req.method === 'PUT') {
+      // Handle PUT request
+      req.body = JSON.parse(req.body);
+      const item = req.body.item ? req.body.item.trim() : null;
+      let imgUrl = req.body.imgUrl ? req.body.imgUrl.trim() : null;
+      const description = req.body.description ? req.body.description.trim() : null;
+      const url = req.body.url ? req.body.url.trim() : null;
+      let id = 0;
+
+      if (isNaN(req.query.id)) {
+        res.status(400).send('ID must be a number');
+        return;
+      } else id = Number(req.query.id);
+
+      if (!item) {
+        res.status(400).send('Name required');
+        return;
+      }
+
+      try {
+        const db_item = await prisma.item.findUnique({
+          where: {
+            id,
+          },
+        });
+
+        const updated = await prisma.item.update({
+          data: {
+            item,
+            imgUrl,
+            description,
+            url,
+          },
+          where: {
+            id,
+          },
+        });
+        res.status(200).json(updated);
+      } catch (e) {
+        console.log(e);
+        res.status(500).send('Server Error');
+      }
+    } else if (req.method === 'GET') {
+      // Handle GET request
+      let id = 0;
+
+      if (isNaN(req.query.id)) {
+        res.status(400).send('ID must be a number');
+        return;
+      } else id = Number(req.query.id);
+
+      const item = await prisma.item.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!item) {
+        res.status(400).send('Invalid ID');
+        return;
+      }
+
+      res.status(200).json(item);
+    } else if (req.method === 'DELETE') {
+      // Handle DELETE request
+      if (isNaN(req.query.id) || !req.query.id) {
+        res.status(400).send('Invalid ID');
+        return;
+      }
+      const id = Number(req.query.id);
+
+      try {
+        const item = await prisma.item.findUnique({
+          where: {
+            id,
+          },
+        });
 
         if (!item) {
-            res.status(400).send('Name required');
-            return;
+          res.status(400).send('Invalid ID');
+          return;
         }
 
-        if (req.cookies.token) await jwt.verify(req.cookies.token, process.env.JWT_SECRET, async function (err, decoded) {
-            if (!decoded.id) {
-                res.status(403).send('Not Authorized');
-                return;
-            }
-
-            try {
-                const db_item = await prisma.item.findUnique({
-                    where: {
-                        id
-                    }
-                });
-                if (db_item.userId !== decoded.id) {
-                    res.status(403).send('Not Authorized');
-                    return;
-                }
-                const updated = await prisma.item.update({
-                    data: {
-                        item,
-                        imgUrl,
-                        description,
-                        url
-                    },
-                    where: {
-                        id
-                    }
-                })
-                res.status(200).json(updated);
-            } catch (e) {
-                console.log(e);
-                res.status(500).send('Server Error');
-            }
-
+        await prisma.item.delete({
+          where: {
+            id,
+          },
         });
-        else res.status(403).send('Not Authorized');
-    } else if (req.method === 'GET') {
-        let id = 0;
 
-        if (isNaN(req.query.id)) {
-            res.status(400).send('ID must be a number');
-            return;
-        } else id = Number(req.query.id);
-
-        if (req.cookies.token) await jwt.verify(req.cookies.token, process.env.JWT_SECRET, async function(err, decoded) {
-            if (!decoded.id) {
-                res.status(403).send('Not Authorized');
-                return;
-            }
-
-            const item = await prisma.item.findUnique({
-                where: {
-                    id
-                }
-            });
-
-            if (!item) {
-                res.status(400).send('Invalid ID');
-                return;
-            }
-
-            if (item.userId !== decoded.id) {
-                res.status(403).send('Not Authorized');
-                return;
-            }
-
-            res.status(200).json(item);
-        });
-        else res.status(403).send('Not Authorized');
-    } else if (req.method === 'DELETE') {
-        if (isNaN(req.query.id) || !req.query.id) {
-            res.status(400).send('Invalid ID');
-            return;
-        }
-        const id = Number(req.query.id);
-
-        if (req.cookies.token) await jwt.verify(req.cookies.token, process.env.JWT_SECRET, async function(err, decoded) {
-            try {
-                if (!decoded.id) {
-                    res.status(403).send('Not Authorized');
-                    return;
-                }
-
-                const item = await prisma.item.findUnique({
-                    where: {
-                        id
-                    }
-                });
-
-                if (!item) {
-                    res.status(400).send('Invalid ID');
-                    return;
-                }
-
-                if (item.userId !== decoded.id) {
-                    res.status(403).send('Not Authorized');
-                    return;
-                }
-
-                await prisma.item.delete({
-                    where: {
-                        id
-                    }
-                });
-
-                res.status(200).send('success');
-            } catch (e) {
-                console.log(e);
-                res.status(500).send('Server Error');
-            }
-        });
-        else res.status(403).send('Not Authorized');
-    } else res.status(400).send('Bad Request');
+        res.status(200).send('success');
+      } catch (e) {
+        console.log(e);
+        res.status(500).send('Server Error');
+      }
+    } else {
+      res.status(400).send('Bad Request');
+    }
+  }
 }
