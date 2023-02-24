09e4d7bf2303a8c17846b055c5a368e49ecf6e77
Jen Perez <jenperez613@icloud.com>
2023-02-24 06:58:30 -0500

    chatgpt told me how to do all of this

--- pages/api/items/[id]/index.js
+++ pages/api/items/[id]/index.js
@@ -9,58 +9,80 @@ export default async function handler(req, res) {
   }
   import prisma from '@/lib/prisma';
 
-  export default async function handler(req, res) {
-    if (req.method === 'PUT') {
-      // Handle PUT request
-      req.body = JSON.parse(req.body);
-      const item = req.body.item ? req.body.item.trim() : null;
-      let imgUrl = req.body.imgUrl ? req.body.imgUrl.trim() : null;
-      const description = req.body.description ? req.body.description.trim() : null;
-      const url = req.body.url ? req.body.url.trim() : null;
-      let id = 0;
+  if (req.method === 'PUT') {
+    // Handle PUT request
+    req.body = JSON.parse(req.body);
+    const item = req.body.item ? req.body.item.trim() : null;
+    let imgUrl = req.body.imgUrl ? req.body.imgUrl.trim() : null;
+    const description = req.body.description
+      ? req.body.description.trim()
+      : null;
+    const url = req.body.url ? req.body.url.trim() : null;
+    let id = 0;
 
-      if (isNaN(req.query.id)) {
-        res.status(400).send('ID must be a number');
-        return;
-      } else id = Number(req.query.id);
+    if (isNaN(req.query.id)) {
+      res.status(400).send('ID must be a number');
+      return;
+    } else id = Number(req.query.id);
 
-      if (!item) {
-        res.status(400).send('Name required');
-        return;
-      }
+    if (!item) {
+      res.status(400).send('Name required');
+      return;
+    }
 
-      try {
-        const db_item = await prisma.item.findUnique({
-          where: {
-            id,
-          },
-        });
+    try {
+      const db_item = await prisma.item.findUnique({
+        where: {
+          id,
+        },
+      });
 
-        const updated = await prisma.item.update({
-          data: {
-            item,
-            imgUrl,
-            description,
-            url,
-          },
-          where: {
-            id,
-          },
-        });
-        res.status(200).json(updated);
-      } catch (e) {
-        console.log(e);
-        res.status(500).send('Server Error');
-      }
-    } else if (req.method === 'GET') {
-      // Handle GET request
-      let id = 0;
+      const updated = await prisma.item.update({
+        data: {
+          item,
+          imgUrl,
+          description,
+          url,
+        },
+        where: {
+          id,
+        },
+      });
+      res.status(200).json(updated);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send('Server Error');
+    }
+  } else if (req.method === 'GET') {
+    // Handle GET request
+    let id = 0;
 
-      if (isNaN(req.query.id)) {
-        res.status(400).send('ID must be a number');
-        return;
-      } else id = Number(req.query.id);
+    if (isNaN(req.query.id)) {
+      res.status(400).send('ID must be a number');
+      return;
+    } else id = Number(req.query.id);
 
+    const item = await prisma.item.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!item) {
+      res.status(400).send('Invalid ID');
+      return;
+    }
+
+    res.status(200).json(item);
+  } else if (req.method === 'DELETE') {
+    // Handle DELETE request
+    if (isNaN(req.query.id) || !req.query.id) {
+      res.status(400).send('Invalid ID');
+      return;
+    }
+    const id = Number(req.query.id);
+
+    try {
       const item = await prisma.item.findUnique({
         where: {
           id,
@@ -72,40 +94,18 @@ export default async function handler(req, res) {
         return;
       }
 
-      res.status(200).json(item);
-    } else if (req.method === 'DELETE') {
-      // Handle DELETE request
-      if (isNaN(req.query.id) || !req.query.id) {
-        res.status(400).send('Invalid ID');
-        return;
-      }
-      const id = Number(req.query.id);
+      await prisma.item.delete({
+        where: {
+          id,
+        },
+      });
 
-      try {
-        const item = await prisma.item.findUnique({
-          where: {
-            id,
-          },
-        });
-
-        if (!item) {
-          res.status(400).send('Invalid ID');
-          return;
-        }
-
-        await prisma.item.delete({
-          where: {
-            id,
-          },
-        });
-
-        res.status(200).send('success');
-      } catch (e) {
-        console.log(e);
-        res.status(500).send('Server Error');
-      }
-    } else {
-      res.status(400).send('Bad Request');
+      res.status(200).send('success');
+    } catch (e) {
+      console.log(e);
+      res.status(500).send('Server Error');
     }
+  } else {
+    res.status(400).send('Bad Request');
   }
 }
